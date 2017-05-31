5dc1022ec3059322960a6a66971020eb424353a3
Ben Chatelain <ben@octop.ad>
2017-05-31 10:45:37 -0600

    Add quick dependency

--- Package.swift
+++ Package.swift
@@ -3,5 +3,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "Butler"
+    name: "Butler",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1)
+    ],
+    // {
+    //     return [
+    //         Target(name: "Butler"),
+    //         Target(name: "ButlerTests", dependencies: [
+    //         ]),
+    //     ]
+    // }(),
+    swiftLanguageVersions: [3]
 )
