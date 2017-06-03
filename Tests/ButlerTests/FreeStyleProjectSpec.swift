b8d4beccd539a9f8312befb726acc9cb06d5b812
Ben Chatelain <ben@octop.ad>
2017-06-02 19:34:19 -0600

    Add stub for FreeStyleProjectSpec

---
+++ Tests/ButlerTests/FreeStyleProjectSpec.swift
@@ -0,0 +1,22 @@
+//
+//  FreeStyleProjectSpec.swift
+//  Butler
+//
+//  Created by Ben Chatelain on 6/2/17.
+//
+//
+
+@testable import Butler
+import Quick
+import Nimble
+
+class FreeStyleProjectSpec: QuickSpec {
+    override func spec() {
+        describe("freestyle project job") {
+            beforeEach {}
+            it("") {
+                expect(true) == true
+            }
+        }
+    }
+}
