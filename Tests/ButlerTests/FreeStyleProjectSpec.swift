f401bdb20436fefe7404707f696359a5cc93eb5a
Ben Chatelain <ben@octop.ad>
2017-06-03 14:47:29 -0600

    Add first JSON parsing tests

--- Tests/ButlerTests/FreeStyleProjectSpec.swift
+++ Tests/ButlerTests/FreeStyleProjectSpec.swift
@@ -9,13 +9,29 @@
 @testable import Butler
 import Quick
 import Nimble
+import SwiftyJSON
 
 class FreeStyleProjectSpec: QuickSpec {
+
     override func spec() {
         describe("freestyle project job") {
-            beforeEach {}
-            it("") {
-                expect(true) == true
+            let jsonFile: NSString = "FreeStyleProject.json"
+            var json: JSON! = nil
+
+            beforeEach {
+                let bundle = Bundle(for: type(of: self)) //.resourceURL!.appendingPathComponent("JSON/Job")
+                let url = bundle.url(forResource: jsonFile.deletingPathExtension,
+                                       withExtension: jsonFile.pathExtension,
+                                    subdirectory: "JSON/Job")!
+                let data = NSData(contentsOf: url)!
+                json = JSON(data: data as Data)
+            }
+
+            it("is buildable") {
+                expect(json["buildable"]) == true
+            }
+            it("is blue") {
+                expect(json["color"]) == "blue"
             }
         }
     }
