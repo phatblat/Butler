f54848e585f018f91e2c19372dbb53e3b5c5a37d
Ben Chatelain <ben@octop.ad>
2017-06-04 17:55:12 -0600

    Move parsing into FreeStyleProject constructor

--- Sources/FreeStyleProject.swift
+++ Sources/FreeStyleProject.swift
@@ -6,6 +6,7 @@
 //
 //
 
+import SwiftyJSON
 import Foundation
 
 struct FreeStyleProject {
@@ -19,17 +20,16 @@ struct FreeStyleProject {
 
     let nextBuildNumber: Int
 
+    let color: String
     let description: String
     let displayName: String
     let fullDisplayName: String
     let fullName: String
-    let healthReport: String
     let name: String
-    let property: String
     let queueItem: String?
     let scm: String
 
-    let builds: [String]
+    let firstBuild: String?
     let lastBuild: String?
     let lastCompletedBuild: String?
     let lastFailedBuild: String?
@@ -38,6 +38,70 @@ struct FreeStyleProject {
     let lastUnstableBuild: String?
     let lastUnsuccessfulBuild: String?
 
+    let healthReport: String
+    let property: [String]
+    let builds: [String]
     let upstreamProjects: [String]
     let downstreamProjects: [String]
+}
+
+extension FreeStyleProject {
+    init?(json plainJson: JSONDictionary) {
+        let json = JSON(plainJson)
+
+        // Required properties
+        guard let _class = json["_class"].string,
+            let url = json["url"].url,
+            let buildable = json["buildable"].bool,
+            let concurrentBuild = json["concurrentBuild"].bool,
+            let color = json["color"].string,
+            let description = json["description"].string,
+            let displayName = json["displayName"].string,
+            let fullDisplayName = json["fullDisplayName"].string,
+            let fullName = json["fullName"].string,
+            let healthReport = json["healthReport"][0]["description"].string,
+            let inQueue = json["inQueue"].bool,
+            let keepDependencies = json["keepDependencies"].bool,
+            let name = json["name"].string,
+            let nextBuildNumber = json["nextBuildNumber"].int,
+            let scm = json["scm"]["_class"].string
+        else {
+            return nil
+        }
+
+        self._class = _class
+        self.url = url
+        self.buildable = buildable
+        self.color = color
+        self.concurrentBuild = concurrentBuild
+        self.description = description
+        self.displayName = displayName
+        self.fullDisplayName = fullDisplayName
+        self.fullName = fullName
+        self.healthReport = healthReport
+        self.inQueue = inQueue
+        self.keepDependencies = keepDependencies
+        self.name = name
+        self.nextBuildNumber = nextBuildNumber
+        self.scm = scm
+
+        // TEMP: Extracting only the url until we have a Build model
+        builds = json["builds"].arrayValue.map({$0["url"].stringValue})
+        upstreamProjects = json["upstreamProjects"].arrayValue.map({$0["url"].stringValue})
+        downstreamProjects = json["downstreamProjects"].arrayValue.map({$0["url"].stringValue})
+
+        // TEMP: Need example of real properties
+//        self.property = json["property"].array
+        property = []
+
+        queueItem = json["queueItem"].string
+        firstBuild = json["firstBuild"]["url"].string
+        lastBuild = json["lastBuild"]["url"].string
+        lastCompletedBuild = json["lastCompletedBuild"]["url"].string
+        lastFailedBuild = json["lastFailedBuild"]["url"].string
+        lastStableBuild = json["lastStableBuild"]["url"].string
+        lastSuccessfulBuild = json["lastSuccessfulBuild"]["url"].string
+        lastUnstableBuild = json["lastUnstableBuild"]["url"].string
+        lastUnsuccessfulBuild = json["lastUnsuccessfulBuild"]["url"].string
+    }
 }
--- Tests/ButlerTests/FreeStyleProjectSpec.swift
+++ Tests/ButlerTests/FreeStyleProjectSpec.swift
@@ -9,108 +9,114 @@
 @testable import Butler
 import Quick
 import Nimble
-import SwiftyJSON
 
 class FreeStyleProjectSpec: QuickSpec {
     override func spec() {
         describe("freestyle project job") {
             let jsonFile: NSString = "FreeStyleProject.json"
-            var json: JSON! = nil
+            var project: FreeStyleProject! = nil
 
             beforeEach {
-                let bundle = Bundle(for: type(of: self)) //.resourceURL!.appendingPathComponent("JSON/Job")
+                let bundle = Bundle(for: type(of: self))
                 let url = bundle.url(forResource: jsonFile.deletingPathExtension,
                                        withExtension: jsonFile.pathExtension,
                                     subdirectory: "JSON/Job")!
                 let data = NSData(contentsOf: url)!
-                json = JSON(data: data as Data)
+
+                let json = try! JSONSerialization.jsonObject(with: data as Data, options: []) as! JSONDictionary
+                project = FreeStyleProject(json: json)
             }
 
             it("has class") {
-                expect(json["_class"]) == "hudson.model.FreeStyleProject"
+                expect(project._class) == "hudson.model.FreeStyleProject"
             }
             it("has a url") {
-                expect(json["url"]) == "http://mini.log-g.co/jenkins/job/whoami/"
+                expect(project.url) == URL(string: "http://mini.log-g.co/jenkins/job/whoami/")
             }
             it("is buildable") {
-                expect(json["buildable"]) == true
+                expect(project.buildable) == true
             }
             it("is blue") {
-                expect(json["color"]) == "blue"
+                expect(project.color) == "blue"
             }
             it("is not concurrent") {
-                expect(json["concurrentBuild"]) == false
+                expect(project.concurrentBuild) == false
             }
             it("has an empty description") {
-                expect(json["description"]) == ""
+                expect(project.description) == ""
             }
             it("has a name") {
-                expect(json["name"]) == "whoami"
+                expect(project.name) == "whoami"
             }
             it("has a full name") {
-                expect(json["fullName"]) == "whoami"
+                expect(project.fullName) == "whoami"
             }
             it("has a display name") {
-                expect(json["displayName"]) == "whoami"
+                expect(project.displayName) == "whoami"
             }
             it("has a full display name") {
-                expect(json["fullDisplayName"]) == "whoami"
+                expect(project.fullDisplayName) == "whoami"
             }
             it("has 2 builds") {
-                expect(json["builds"].count) == 2
+                expect(project.builds.count) == 2
             }
             it("has a next build number") {
-                expect(json["nextBuildNumber"]) == 3
+                expect(project.nextBuildNumber) == 3
             }
             it("has no scm") {
-                expect(json["scm"]["_class"]) == "hudson.scm.NullSCM"
+                expect(project.scm) == "hudson.scm.NullSCM"
             }
             it("has no properties") {
-                expect(json["property"]) == []
+                expect(project.property) == []
             }
             it("has no downstream projects") {
-                expect(json["downstreamProjects"]) == []
+                expect(project.downstreamProjects) == []
             }
             it("has no uptream projects") {
-                expect(json["upstreamProjects"]) == []
+                expect(project.upstreamProjects) == []
             }
             it("has a health report") {
-                expect(json["healthReport"]).notTo(beNil())
-                expect(json["healthReport"][0]["score"]) == 100
+                expect(project.healthReport).notTo(beNil())
+                // description
+                expect(project.healthReport) == "Build stability: No recent builds failed."
+//                expect(project.healthReport[0]["score"]) == 100
             }
             it("is not in the queue") {
-                expect(json["inQueue"]) == false
+                expect(project.inQueue) == false
             }
             it("has no queue item") {
-                expect(json["queueItem"].string).to(beNil())
-                expect(json["queueItem"].exists()) == true
-                expect(json["queueItem"].isEmpty) == true
-                expect(json["queueItem"].null) == NSNull()
-                expect(json["queueItem"].type) == SwiftyJSON.Type.null
+                expect(project.queueItem).to(beNil())
             }
             it("does not keep dependencies") {
-                expect(json["keepDependencies"]) == false
+                expect(project.keepDependencies) == false
             }
             it("has a last build") {
-                expect(json["lastBuild"]["number"]) == 2
+//                expect(project.lastBuild["number"]) == 2
+                expect(project.lastBuild) == "http://mini.log-g.co/jenkins/job/whoami/2/"
             }
             it("has a last complete build") {
-                expect(json["lastCompletedBuild"]["number"]) == 2
+//                expect(project.lastCompletedBuild["number"]) == 2
+                expect(project.lastCompletedBuild) == "http://mini.log-g.co/jenkins/job/whoami/2/"
             }
             it("has a last stable build") {
-                expect(json["lastStableBuild"]["number"]) == 2
+//                expect(project.lastStableBuild["number"]) == 2
+                expect(project.lastStableBuild) == "http://mini.log-g.co/jenkins/job/whoami/2/"
             }
             it("has a last successful build") {
-                expect(json["lastSuccessfulBuild"]["number"]) == 2
+//                expect(project.lastSuccessfulBuild["number"]) == 2
+                expect(project.lastSuccessfulBuild) == "http://mini.log-g.co/jenkins/job/whoami/2/"
             }
             it("does not have a last failed build") {
-                expect(json["lastFailedBuild"]["number"].int).to(beNil())
+//                expect(project.lastFailedBuild["number"].int).to(beNil())
+                expect(project.lastFailedBuild).to(beNil())
             }
             it("does not have a last unstable build") {
-                expect(json["lastUnstableBuild"]["number"].int).to(beNil())
+//                expect(project.lastUnstableBuild["number"].int).to(beNil())
+                expect(project.lastUnstableBuild).to(beNil())
             }
             it("does not have a last unsuccessful build") {
-                expect(json["lastUnsuccessfulBuild"]["number"].int).to(beNil())
+//                expect(project.lastUnsuccessfulBuild["number"].int).to(beNil())
+                expect(project.lastUnsuccessfulBuild).to(beNil())
             }
         }
     }
