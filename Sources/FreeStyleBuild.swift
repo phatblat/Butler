e149936b1e8093bed71d7ab85886cfbd39cc1fe8
Ben Chatelain <ben@octop.ad>
2017-06-09 13:19:36 -0600

    Add FreeStyleBuild

---
+++ Sources/FreeStyleBuild.swift
@@ -0,0 +1,14 @@
+//
+//  FreeStyleBuild.swift
+//  Butler
+//
+//  Created by Ben Chatelain on 6/9/17.
+//
+
+import Foundation
+
+struct FreeStyleBuild: Codable {
+    let _class: String
+    let number: Int
+    let url: URL
+}
