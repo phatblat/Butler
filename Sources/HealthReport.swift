d3b4761d748359d1d10732a48ca87e10518e826b
Ben Chatelain <ben@octop.ad>
2017-06-09 13:26:41 -0600

    Add HealthReport

---
+++ Sources/HealthReport.swift
@@ -0,0 +1,13 @@
+//
+//  HealthReport.swift
+//  Butler
+//
+//  Created by Ben Chatelain on 6/9/17.
+//
+
+import Foundation
+
+struct HealthReport: Codable {
+    let description: String
+    let score: Int
+}
